d5c7cb43997f79a835f51e4985e24b45d792818b
Juan Campos <juanmcm2oo1@gmail.com>
2021-09-14 10:57:47 -0500

    Modified: comments added

--- Ejercicio02.dart
+++ Ejercicio02.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
-void main(){
-   double kilogramo;
-   double gramo;
+
+void main() {
+  double kilogramo;
+  double gramo;
   print('PROGRAMA PARA CONVERTIR DE KILOGRAMOS A GRAMOS:');
+  // Comando para pedir al usuario que ingrese el valor a convertir
   kilogramo = double.parse(stdin.readLineSync()!);
-  gramo = kilogramo*1000;
+  // Conversión de kilogramo a gramo
+  gramo = kilogramo * 1000;
   print("La cantidad de gramos es: ");
   print(gramo);
-
 }
